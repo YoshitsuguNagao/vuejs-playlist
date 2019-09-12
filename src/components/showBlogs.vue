48fc90b7f7840c25e96555043591620acaeea72c
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-09-13 00:11:36 +0200

    search bar

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
-    <div class="single-blog" v-for="blog in blogs" v-bind:key="blog.id">
+    <input type="text" v-model="search" placeholder="search blogs">
+    <div class="single-blog" v-for="blog in filteredBlogs" v-bind:key="blog.id">
       <h2 v-rainbow>{{blog.title|to-uppercase}}</h2>
       <article>{{blog.body|snippet}}</article>
     </div>
@@ -9,29 +10,37 @@
 </template>
 
 <script>
-export default {
+  export default {
 
-  data () {
-    return {
-      blogs: []
+    data () {
+      return {
+        blogs: [],
+        search: ""
+      }
+    },
+    methods: {
+
+    },
+    created() {
+      this.$http.get('https://jsonplaceholder.typicode.com/posts')
+        .then(function(data) {
+          this.blogs = data.body.slice(0,10);
+        })
+    },
+    computed: {
+      filteredBlogs: function() {
+        return this.blogs.filter((blog) => {
+          return blog.title.match(this.search);
+        })
+      }
     }
-  },
-  methods: {
-
-  },
-  created() {
-    this.$http.get('https://jsonplaceholder.typicode.com/posts')
-      .then(function(data) {
-        this.blogs = data.body.slice(0,10);
-      })
   }
-}
 </script>
 
 <style>
   #show-blogs {
     max-width: 800px;
-    margin: 0 auto;
+    margin: 0px auto;
   }
   .single-blog {
     padding: 20px;
